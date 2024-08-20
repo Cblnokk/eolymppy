afb0ec3cc29d18559acbb1bf77d358e9e7c6bcd2
Cblnokk <97527919+Cblnokk@users.noreply.github.com>
2024-08-20 18:09:40 +0200

    Add files via upload

---
+++ dz1/main.py
@@ -0,0 +1,13 @@
+from turtle import *
+shape("turtle")
+pensize(100000)
+forward(100)
+left(90)
+forward(100)
+penup()
+color("red", "yellow")
+pendown()
+left(90)
+forward(100)
+left(180)
+forward(150)
